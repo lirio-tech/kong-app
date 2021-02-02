e2c0f31a8aa6cc2c391b3e777001931979df888d
Diego Lirio <diegolirio.dl@gmail.com>
2021-02-01 23:55:01 -0300

    PWA

--- src/main.js
+++ src/main.js
@@ -6,10 +6,11 @@ import VueMask from 'v-mask'
 import './plugins/axiosRequestIntercept'
 import { VueMaskDirective } from 'v-mask'
 import { VueMaskFilter } from 'v-mask'
+import './registerServiceWorker'
 
 
 Vue.config.productionTip = false
-Vue.use(VueMask); 
+Vue.use(VueMask);
 Vue.directive('mask', VueMaskDirective);
 Vue.filter('VMask', VueMaskFilter);
 
