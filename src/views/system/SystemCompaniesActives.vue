922cd7e0cb2d6be5df0c3b5edf6613625d57935b
Diego Lirio <diegolirio.dl@gmail.com>
2021-03-18 21:20:55 -0300

    Companies Actives

--- src/views/system/SystemCompaniesActives.vue
+++ src/views/system/SystemCompaniesActives.vue
@@ -16,51 +16,71 @@
                 </p>  
               </v-col>
           </v-row>     
+          <v-dialog
+            v-model="loading"
+            hide-overlay
+            persistent
+            width="300"
+          >
+            <v-card
+              color="primary"
+              dark
+            >
+              <v-card-text>
+                Por favor aguarde...
+                <v-progress-linear
+                  indeterminate
+                  color="white"
+                  class="mb-0"
+                ></v-progress-linear>
+              </v-card-text>
+            </v-card>
+          </v-dialog>          
           <v-row>
-          <v-col
-                cols="12"
-                sm="6"
-                md="4"
-              >
-                <v-dialog
-                  ref="dialog"
-                  v-model="modal"
-                  :return-value.sync="date" 
-                  persistent
-                  width="290px"
+            <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
                 >
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-text-field
-                      v-model="datesDisplay"
-                      label="Escolha o Periodo"
-                      prepend-icon="mdi-calendar"
-                      readonly
-                      v-bind="attrs"
-                      v-on="on"
-                    ></v-text-field>
-                  </template>
-                  <v-date-picker
-                    v-model="dates"
-                    range
+                  <v-dialog
+                    ref="dialog"
+                    v-model="modal"
+                    :return-value.sync="date" 
+                    persistent
+                    width="290px"
                   >
-                    <v-spacer></v-spacer>
-                    <v-btn
-                      text
-                      color="primary"
-                      @click="modal = false"
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-text-field
+                        v-model="datesDisplay"
+                        label="Escolha o Periodo"
+                        prepend-icon="mdi-calendar"
+                        readonly
+                        v-bind="attrs"
+                        v-on="on"
+                      ></v-text-field>
+                    </template>
+                    <v-date-picker
+                      v-model="dates"
+                      range
                     >
-                      Cancel
-                    </v-btn>
-                    <v-btn
-                      text
-                      color="primary"
-                      @click="$refs.dialog.save(date); getCompaniesActives(dates)"
-                    >
-                      OK
-                    </v-btn>
-                  </v-date-picker>
-                </v-dialog>
-          </v-col>     
+                      <v-spacer></v-spacer>
+                      <v-btn
+                        text
+                        color="primary"
+                        @click="modal = false"
+                      >
+                        Cancel
+                      </v-btn>
+                      <v-btn
+                        text
+                        color="primary"
+                        @click="$refs.dialog.save(date); getCompaniesActives(dates)"
+                      >
+                        OK
+                      </v-btn>
+                    </v-date-picker>
+                  </v-dialog>
+            </v-col>     
           </v-row> 
           <v-row> 
             <v-col cols="12">
@@ -83,6 +103,7 @@ export default {
     },
     data: () => ({
       loading: false,
+      dataReturnOK: true,
       userLogged: { 
         type: 'none'
       },
@@ -92,14 +113,17 @@ export default {
     }),
     methods: {
       getCompaniesActives(dates) {
-      gateway.getAnalyticsCompaniesActives(dates,
-        res => {
-            this.loading = false;
-            this.companies = res;
-        }, err => {
-            console.log(err);
-            this.loading = false;
-        });           
+        this.loading = true;
+        gateway.getAnalyticsCompaniesActives(dates,
+          res => {
+              this.dataReturnOK = true;
+              this.loading = false;
+              this.companies = res;
+          }, () => {
+              this.dataReturnOK = false;
+              this.loading = false;
+              alert('Erro ao Buscar Estabelecimentos Ativos');
+          });           
       }
     },
     beforeMount() {
