40c5cf23aaa253b5bedacf3ce7b3a949ef5956a4
Diego Lirio <diegolirio.dl@gmail.com>
2021-03-31 14:49:56 -0300

    +

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -148,6 +148,88 @@
                           <v-btn 
                               v-if="!loading"
                               :to="{ path:'/ordem-servico'}" 
+                              large
+                              style="width: 70%"
+                              class="ma-2"
+                              outlined
+                              color="cyan"
+                          >
+                            <v-icon style="margin-right: 10px;">
+                              mdi-content-cut
+                            </v-icon>
+                            Novo
+                          </v-btn>                                            
+                        </v-col>  
+                    </v-card-actions>
+            </v-card>   
+           <v-card
+              class="mx-auto"
+              max-width="800"
+              outlined
+              v-else
+            >
+                    <v-list-item three-line>
+                      <v-list-item-content>       
+                            <v-col cols="11" style="margin-top: -20px;margin-left: -10px;">
+                              <div class="overline mb-4 grey--text">
+                                  Periodo: 
+                                  <span class="">{{ consolidado.periodoDescricao }}</span>
+                              </div>                           
+                            </v-col>
+                            <v-col cols="1" v-if="isAdmin()">
+                              <span style="margin-left: -15px;">
+                                  <router-link to="/analytics" style="color: inherit; text-decoration: none">
+                                      <v-btn fab x-small outlined color="cyan">
+                                          <v-icon>
+                                              mdi-chart-bar
+                                          </v-icon>    
+                                      </v-btn>
+                                  </router-link>                          
+                              </span>     
+                            </v-col>                            
+                              
+                            <v-col cols="7">
+                              <v-list-item-title class="headline mb-1" style="margin-top: -35px;">
+                                  <span class="caption grey--text">Total de {{ userLogged.name.split(" ")[0] }}</span><br/>
+                                  <div v-if="!loading" style="font-size: 1.7rem">
+                                    <span class="green--text">{{ consolidado.total | currency }} </span>
+                                  </div>
+                              </v-list-item-title>
+                            </v-col>
+                            <v-col cols="4">
+                              <v-list-item-title class="headline mb-1 text-center" style="margin-top: -35px;">
+                                  <span class="caption grey--text">Qtde.</span><br/>
+                                  <div v-if="!loading" style="font-size: 1.7rem">
+                                    <span class="green--text">{{ orders.length }} </span>
+                                    <br/>
+                                  </div>
+                              </v-list-item-title>
+                            </v-col>                            
+                            <hr style="border: 1px dotted #424242;border-radius: 5px;" />
+                            <v-col cols="6" style="margin-top:5px" class="text-center">
+                                <v-icon color="green" style="margin-top: -5px">
+                                  mdi-cash
+                                </v-icon> 
+                                <span class="grey--text" style="font-size: 1.2rem">
+                                  {{ sumPaymentType.cash | currency }}
+                                </span>
+                            </v-col>
+                            <v-col cols="6" style="margin-top:5px" class="text-center">
+                                <v-icon color="purple" style="margin-top: -5px">
+                                  mdi-credit-card
+                                </v-icon>                                    
+                                <span class="grey--text" style="font-size: 1.2rem">
+                                  {{ sumPaymentType.card | currency }}
+                                </span>                                
+                            </v-col> 
+                            <hr v-if="orders.length" style="margin-top: 15px; border: 1px dotted #424242;border-radius: 5px;" />
+                      </v-list-item-content>
+                    </v-list-item>
+                    <v-card-actions > 
+                        <v-col cols="12" class="text-center" style="margin-top: -15px;">
+                          <v-btn 
+                              v-if="!loading"
+                              :to="{ path:'/ordem-servico'}" 
                               class="ma-2"
                               large
                               style="width: 70%"
@@ -161,8 +243,8 @@
                           </v-btn>                                            
                         </v-col>  
                     </v-card-actions>
-            </v-card>   
-            <v-card
+            </v-card>               
+            <!-- <v-card
               class="mx-auto"
               max-width="800"
               outlined
@@ -234,7 +316,7 @@
                           >Novo</v-btn>                                            
                         </v-col>  
                     </v-card-actions>
-            </v-card>   
+            </v-card>    -->
             
             <br/>
 
