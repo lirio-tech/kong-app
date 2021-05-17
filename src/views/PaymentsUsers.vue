6a1da7054d455b9502e08a76bfa4bb60d0c967e8
Diego Lirio <diegolirio.dl@gmail.com>
2021-05-17 00:19:00 -0300

    List Cards UserPaymentsOrServiceReceive

--- src/views/PaymentsUsers.vue
+++ src/views/PaymentsUsers.vue
@@ -18,18 +18,70 @@
           </v-row>          
             <v-row>
                 <v-col cols="12" sm="12">
-                    <v-sheet min-height="70vh" rounded="lg">
-                        <v-data-table 
-                            :headers="headers" 
-                            :items="usersPayments" 
-                            item-key="code"
-                            class="elevation-1"
-                            hide-default-footer
-                            loading-text="Carregando... Por favor aguarde"
-                            @click:row="clickRow"
-                        >                                                  
-                        </v-data-table>               
-                    </v-sheet>
+                    <v-card
+                      class="mx-auto"
+                      max-width="800"
+                      outlined
+                    >
+                            <v-list-item three-line>
+                              <v-list-item-content>                                  
+                                    <br/>
+                                    <div class="grey--text">
+                                        <span class="">Comissão a Receber</span>
+                                    </div>          
+                                    <br/>
+                                    <hr style="border: 1px dotted #424242;border-radius: 5px;" />   
+                                    <br/>                    
+                                    <v-row 
+                                      v-for="userPay in usersPayments" 
+                                      :key="userPay" 
+                                      style="margin-top:-5px"
+                                    >
+                                        <v-col cols="7" style="font-size: 1.1rem">
+                                              {{ userPay.name }}
+                                        </v-col> 
+                                        <v-col cols="5" :style="'font-size: 1.1rem;'">
+                                              {{ userPay.valueReceive | currency }}
+                                        </v-col>
+                                        <v-col cols="12" class="text-center">
+                                          <v-btn 
+                                              :to="{ path:'/payments-users'}" 
+                                              large
+                                              style="width: 90%"
+                                              class="ma-2"
+                                              outlined
+                                              color="cyan"
+                                          >
+                                            Ver Vale e Pagamento
+                                          </v-btn>                                            
+                                        </v-col>                    
+                                        <br/>        
+                                        <v-col cols="12" class="text-center" style="margin-top: -15px;">              
+                                          <hr style="border: 1px dotted #424242;border-radius: 5px;" />   
+                                        </v-col>
+                                        <br/>                      
+                                    </v-row>                       
+                              </v-list-item-content>
+                            </v-list-item>
+                            <!-- <v-card-actions > 
+                                <v-col cols="12" class="text-center" style="margin-top: -15px;">
+                                  <v-btn 
+                                      :to="{ path:'/ordem-servico'}" 
+                                      large
+                                      style="width: 70%"
+                                      class="ma-2"
+                                      outlined
+                                      color="cyan"
+                                  >
+                                    <v-icon style="margin-right: 10px;">
+                                      mdi-content-cut
+                                    </v-icon>
+                                    Novo
+                                  </v-btn>                                            
+                                </v-col>  
+                            </v-card-actions> 
+                            -->
+                    </v-card>   
                 </v-col>
             </v-row>              
         </v-main>
@@ -49,7 +101,7 @@ import UserTypes from '../utils/UserTypes';
         { text: "Funcionario", value: "name" },
         { text: "Username", value: "valueReceive" }
       ],                
-      usersPayments: [{name: 'Guilherme', valueReceive: 150.50}]
+      usersPayments: [{name: 'Guilherme', valueReceive: 150.50},{name: 'Guilherme', valueReceive: 150.50}]
     }),
     methods: {
       isAdmin(type) {
