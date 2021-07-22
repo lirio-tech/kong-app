593e067f5dc3262444929448eac16407f2dc17ee
Diego Lirio <83074806+ddamacena@users.noreply.github.com>
2021-07-22 17:54:43 -0300

    009

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -11,7 +11,7 @@
                 </v-col> 
                 <v-col cols="8" align="center" >  
                         <span style="font-size: 2.0rem !important;">{{ appType === 'app' ? 'Kong Barber App' : 'Lady App' }}</span>
-                        <br/><small class="grey--text">Versão: 0.9.8 </small>
+                        <br/><small class="grey--text">Versão: 0.9.9 </small>
                 </v-col>       
             </v-row>   
 
