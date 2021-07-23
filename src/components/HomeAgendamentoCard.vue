81c93262cade2ce7a62e68f330ff88dea9f67cba
Diego Lirio <83074806+ddamacena@users.noreply.github.com>
2021-07-23 18:31:19 -0300

    ..

--- src/components/HomeAgendamentoCard.vue
+++ src/components/HomeAgendamentoCard.vue
@@ -3,17 +3,17 @@
         <v-card
           class="mx-auto"
           max-width="344"
-          color="red lighten-4"
+          :color="getColorSituation(agendamento.status)"
         >
-                    <v-card-text>
-                      <div>Conclua esse agendamento :)</div>
+              <v-card-text>
+                      <div>{{ getTitle(agendamento.status) }}</div>
                       <p class="text-h6 text--primary">
-                        Soraya
+                        {{ agendamento.customer.name }}
                       </p>
-                      <div class="text--primary">
-                        Unha <br/>
-                        23/07/2021 12:00 as 13:00
-                      </div>
+                      Profissional: <b>{{ agendamento.user.name }}</b> <br />
+                      Serviço: <b>{{ getDescriptionServices(agendamento.services) }}</b> <br/>
+                      {{ new Date(agendamento.dateTimeStartAt).toLocaleString('pt-BR').substring(0,10) }} às {{ new Date(agendamento.dateTimeStartAt).toLocaleString('pt-BR').substring(11,16) }} <br/>
+                        
                     </v-card-text>
                     <v-card-actions>
                         <v-btn
@@ -22,7 +22,7 @@
                         >
                           Alterar
                         </v-btn>                        
-                        <v-btn
+                        <v-btn 
                           color="red" 
                           class="white--text"
                         >
@@ -47,15 +47,32 @@ import UserTypes from '../utils/UserTypes'
 export default {
     name: 'HomeAgendamentoCard',
     props: [ 'agendamento', 'userLogged' ],
-    data() {
-      return {
+    // data() {
+    //   return {
         
-      }
-    },
+    //   }
+    // },
     methods: {
       isAdmin() {
         return UserTypes.isAdmin(this.userLogged.type);
       },      
+      getColorSituation(situation) {
+        if(situation === 'PRIOR') return 'red lighten-3';
+        if(situation === 'PENDING') return 'info lighten-1';
+        return ''
+      },
+      getTitle(status){
+        if(status === 'PRIOR') return 'Conclua esse agendamento :)';
+        if(status === 'PENDING') return 'Próximo Agendamento';
+        return ''        
+      },
+        getDescriptionServices(services) {
+            let description = ''
+            for(var i in services) {
+              description += `${services[i].type}, `
+            }
+            return description;
+        },      
     }
   }
 </script>
--- src/components/HomeAgendamentos.vue
+++ src/components/HomeAgendamentos.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="agendamentos.length > 0">
+  <div>
     <div >
         <router-link :to="isAdmin() ? '/admin/agendamentos/' : '/agendamentos/'" style="color: inherit; text-decoration: none">
             <v-card
@@ -9,17 +9,30 @@
             >
                   <v-list-item style="margin-bottom: -35px">
                     <v-list-item-content>                                  
-                            <v-col cols="12" style="margin-top: -20px;margin-left: -10px;">
-                              <div class="overline mb-4 grey--text">
-                                  Agendamentos
-                              </div>                           
-                            </v-col>  
+                        <v-col cols="12" style="margin-top: -20px;margin-left: -10px;">
+                          <div class="overline mb-4 grey--text">
+                              Agendamentos
+                          </div>                           
+                        </v-col>  
                     </v-list-item-content>
                   </v-list-item>            
                   <div v-for="ag in agendamentos" :key="ag._id" style="margin-top: -15px">
-                      <home-agendamento-card :agendamento="agendamento" :userLogged="userLogged" />
+                      <home-agendamento-card :agendamento="ag" :userLogged="userLogged" />
                       <br/>
                   </div>
+                  <div v-if="agendamentos.length == 0" style="margin-top: -15px">
+                      <v-col cols="12">
+                          <span class="grey--text">
+                            Não você não possui nenhum cliente agendado :)
+                          </span>
+                          <br/>
+                          <br/>
+                        <v-btn>
+                          <v-icon color="primary">mdi-clock</v-icon> &nbsp; Agendar
+                        </v-btn>                             
+                      </v-col>
+                  </div>
+                  
             </v-card>      
             
         </router-link>        
