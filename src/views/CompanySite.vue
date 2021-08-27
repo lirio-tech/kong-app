800e9ca84342fb4f00edcd1bf9969d6868b7a608
Diego Lirio <83074806+ddamacena@users.noreply.github.com>
2021-08-27 17:41:33 -0300

    fix

--- src/views/CompanySite.vue
+++ src/views/CompanySite.vue
@@ -236,7 +236,7 @@ export default {
   }),
   methods: {
     isAdmin() {
-      return userLogged && UserTypes.isAdmin(this.userLogged.type);
+      return this.userLogged && UserTypes.isAdmin(this.userLogged.type);
     },    
     openWhats() {
         window.location.href = `https://api.whatsapp.com/send?phone=55${this.companySite.whatsapp}&text=Oi ${this.company.name} 💈, estou no seu site 🙂`
